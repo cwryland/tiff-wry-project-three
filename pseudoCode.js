060065d95e6131972a6d7d5086e0d119a627d7d2
Tiff Wry <tiff.wry@gmail.com>
2020-07-26 22:52:16 -0400

    initial commit :rocket:

---
+++ pseudoCode.js
@@ -0,0 +1,13 @@
+//Trash Panda Express
+
+// 1. User lands on main page with heading "Hello, furry friend!" and reads an introduction about their upcoming journey as a raccoon in Toronto.
+
+//2. Users read their first question and they have two options (A and B) that will be clickable buttons, leading to the next question using a smooth scrolling function.
+
+//3. Users read their second question and again have two choices that will be clickable buttons, again leading to the next section via automatic target scrolling.
+
+//4. Users read the last question and choose from two options, and in the step below I will define what happens to user input data.
+
+//5. I will use a math function to tally the users scores (eg. 2 As, 1 B) to direct them to one of two endings using an if/else statement. Two different endings will have two different stylings and different end blurbs.
+
+//And then we seize the means of production and all is well in the world. The end.
