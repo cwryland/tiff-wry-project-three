6be3b80712173aa126d34b1319e49a28a4fa731b
Tiff Wry <tiff.wry@gmail.com>
2020-07-27 00:08:46 -0400

    fiddling with logic

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ $(function() {
       ending2: 0,
   }
 
-  
+const results = {
+    ending1: {
+        name: "ending1",
+        description: "Congrats, my extroverted furry friend! You like to explore and adventure, taking advantage of everything this great city has to offer!"
+    },
+
+    ending2: {
+        name: "ending2",
+        description: "What a wonderful day in the 6ix. Good eats, good naps, and some introspective time at the beach does a soul good. Enjoy, friend!"
+    },
+
+    const smoothScroll = function(target) {
+      let $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 800, 'swing', function() {
+          window.location.hash = target;
+      });
+  }
